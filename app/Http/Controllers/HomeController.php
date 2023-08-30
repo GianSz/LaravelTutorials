8c1816b179eaad8c3ecde048e3d737a6036d24cb
GianSz <giansz2003@gmail.com>
2023-08-30 01:33:31 -0500

    Tutorial03-A completed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,4 +26,18 @@ class HomeController extends Controller
             ->with('address', $address)
             ->with('phone_number', $phone_number);
     }
+
+    public function about(): view
+    {
+        $data1 = 'About us - Online Store';
+        $data2 = 'About us';
+        $description = 'This is an about page ...';
+        $author = 'Developed by: Gian Paul Sánchez Aristizábal';
+
+        return view('home.about')
+            ->with('title', $data1)
+            ->with('subtitle', $data2)
+            ->with('description', $description)
+            ->with('author', $author);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 
-Route::get('/about', function () {
-    $data1 = 'About us - Online Store';
-    $data2 = 'About us';
-    $description = 'This is an about page ...';
-    $author = 'Developed by: Gian Paul Sánchez Aristizábal';
-
-    return view('home.about')
-        ->with('title', $data1)
-        ->with('subtitle', $data2)
-        ->with('description', $description)
-        ->with('author', $author);
-})->name('home.about');
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.about');
 
 Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name('home.contact');
 
@@ -34,3 +23,7 @@ Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('p
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
 Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
+
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index");
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name("cart.removeAll");
